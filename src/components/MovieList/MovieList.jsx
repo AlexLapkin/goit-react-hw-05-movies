d2aa19c08ba07a907eaec99345796a9bda30585a
AlexLapkin <130095964+AlexLapkin@users.noreply.github.com>
2023-11-14 01:26:57 +0200

    refactoring

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -7,8 +7,14 @@ const MovieList = ({ movieList }) => {
   return (
     <ul className={css.mov_ul_title}>
       {movieList !== null &&
-        movieList.map(({ id, name, title }) => (
-          <MovieListItem key={id} name={name} title={title} id={id} />
+        movieList.map(({ id, name, title, poster_path }) => (
+          <MovieListItem
+            key={id}
+            name={name}
+            title={title}
+            id={id}
+            poster_path={poster_path}
+          />
         ))}
     </ul>
   );
--- src/components/MovieListItem/MovieListItem.jsx
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -4,17 +4,22 @@ import { NavLink } from 'react-router-dom';
 import css from './MovieListItem.module.css';
 import PropTypes from 'prop-types';
 
-const MovieListItem = ({ title, name, id }) => {
+const MovieListItem = ({ title, name, id, poster_path }) => {
   const location = useLocation();
 
   return (
-    <li key={id}>
+    <li className={css.mov_item} key={id}>
       <NavLink
         className={css.mov_title}
         to={`/movies/${id}`}
         state={{ from: location }}
       >
-        {title || name}
+        <img
+          className={css.mov_title_img}
+          src={'https://www.themoviedb.org/t/p/w300' + poster_path}
+          alt=""
+        />
+        <p className={css.mov_text}>{title || name}</p>
       </NavLink>
     </li>
   );
